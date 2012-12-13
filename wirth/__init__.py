5c74f494141cb4d089fb3bef090ceb08db07152e
Xurxo Fresco <xurxof@gmail.com>
2012-12-13 11:05:05 +0100

    Cleanup commented code

--- wirth/__init__.py
+++ wirth/__init__.py
@@ -24,32 +24,3 @@ def isWirth(l):
         if l[-i:] == l[-i*2:-i]:
             return False
     return True;
-
-"""
-
-alphabet=['A','B','C']
-
-def wirth(n):
-    solutions=[]
-    if n==0:
-        return []
-    if n==1:
-        return alphabet
-    
-    for prev in wirth(n-1):
-        
-        for letter in alphabet:
-            current = prev+letter
-            if isWirth(current):
-                solutions.append(current)
-    # print solutions
-    return solutions
-
-def isWirth(l):
-    
-    for i in range(1, (len(l)/2)+1):
-        if l[-i:] == l[-i*2:-i]:
-            return False
-    return True;
-
-"""
